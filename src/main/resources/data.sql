c61d8591ef941ad8a42ede2d9dfc1dc7581631c6
José Ramírez <jota.rf.88@gmail.com>
2025-06-10 12:39:24 -0400

    feat: add database example data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO products (id, name, price, quantity) VALUES (UUID(),'Soap', 3.99, 3);
+INSERT INTO products (id, name, price, quantity) VALUES (UUID(),'Coffee', 1.99, 5);
+INSERT INTO products (id, name, price, quantity) VALUES (UUID(),'Vegetable Oil', 5.99, 7);
+INSERT INTO products (id, name, price, quantity) VALUES (UUID(),'Cookies', 0.99, 10);
+INSERT INTO products (id, name, price, quantity) VALUES (UUID(),'Ice Cream', 2.99, 20);
